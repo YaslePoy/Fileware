2c68479d84575be4835d75352ebdb8516c676065
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-10-05 12:04:50 +0300

    api base

---
+++ FilewareApi/Controllers/FileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilewareApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileController : Controller
+{
+    [HttpPost]
+    public async Task<ActionResult> RegisterNewFile(IFormFile file)
+    {
+        Console.WriteLine(file.FileName);
+        var x = file.Headers;
+        file.CopyTo(Console.OpenStandardOutput());
+        return Ok();
+    }
+}
