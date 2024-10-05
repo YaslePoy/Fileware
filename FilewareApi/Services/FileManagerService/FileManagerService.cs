3cea507e34dff61c2cd56d144d81143f77b7d562
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-10-05 12:28:15 +0300

    база сервиса файлов

---
+++ FilewareApi/Services/FileManagerService/FileManagerService.cs
@@ -0,0 +1,20 @@
+﻿namespace FilewareApi.Services.FileManagerService;
+
+
+public class FileManagerService : IFileManagerService
+{
+    public Guid RegisterNewFile(Stream file, string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetFileSize(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateFile(Guid id, Stream newDataStream)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FilewareApi/Services/FileManagerService/IFileManagerService.cs
@@ -0,0 +1,8 @@
+﻿namespace FilewareApi.Services.FileManagerService;
+
+public interface IFileManagerService
+{
+    Guid RegisterNewFile(Stream file, string name);
+    int GetFileSize(Guid id);
+    void UpdateFile(Guid id, Stream newDataStream);
+}
