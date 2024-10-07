e33c109bca9ea18c59defc65a3efe4882c22dabf
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-10-07 13:06:14 +0300

    drag and drop файлов выглядит красиво

--- Fileware/Views/MainWindow.axaml.cs
+++ Fileware/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
 
 namespace Fileware.Views;
 
@@ -7,5 +10,26 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        AddHandler(DragDrop.DragEnterEvent, DragOver);
+        AddHandler(DragDrop.DragLeaveEvent, DragLeave);
+        AddHandler(DragDrop.DropEvent, DragDropEvent);
+    }
+
+    private void DragOver(object? sender, DragEventArgs e)
+    {
+        Viewer.Effect = new ImmutableBlurEffect(15);
+        LoadPanel.IsVisible = true;
+    }
+
+    private void DragLeave(object? sender, DragEventArgs e)
+    {
+        Viewer.Effect = null;
+        LoadPanel.IsVisible = false;
+    }
+
+    private void DragDropEvent(object? sender, DragEventArgs e)
+    {
+        Viewer.Effect = null;
+        LoadPanel.IsVisible = false;
     }
 }
